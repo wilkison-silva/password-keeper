ad09eabf7f4e49d30caacb46f4f28538019a596e
Wilkison Silva <wilkisons@ciandt.com>
2022-07-25 12:46:46 -0400

    feat: criando usecase de getCardById para retornar para a view um cardView

--- app/src/main/java/br/com/passwordkeeper/domain/usecase/CardUseCase.kt
+++ app/src/main/java/br/com/passwordkeeper/domain/usecase/CardUseCase.kt
@@ -1,12 +1,13 @@
 package br.com.passwordkeeper.domain.usecase
 
 import br.com.passwordkeeper.domain.result.usecase.GetAllCardsUseCaseResult
+import br.com.passwordkeeper.domain.result.usecase.GetCardByIdUseCaseResult
 
 interface CardUseCase {
 
     suspend fun getAllCards(email: String): GetAllCardsUseCaseResult
 
-//    suspend fun getCardById(cardId: String): GetCardByIdRepositoryResult
+    suspend fun getCardById(cardId: String): GetCardByIdUseCaseResult
 //
 //    suspend fun createCard(cardData: CardData, emailUser: String): CreateCardRepositoryResult
 //
--- app/src/main/java/br/com/passwordkeeper/domain/usecase/FirebaseCardUseCaseImpl.kt
+++ app/src/main/java/br/com/passwordkeeper/domain/usecase/FirebaseCardUseCaseImpl.kt
@@ -3,11 +3,14 @@ package br.com.passwordkeeper.domain.usecase
 import br.com.passwordkeeper.data.repository.CardRepository
 import br.com.passwordkeeper.domain.model.CardDomain
 import br.com.passwordkeeper.domain.result.repository.GetAllCardsRepositoryResult
+import br.com.passwordkeeper.domain.result.repository.GetCardByIdRepositoryResult
 import br.com.passwordkeeper.domain.result.usecase.GetAllCardsUseCaseResult
+import br.com.passwordkeeper.domain.result.usecase.GetCardByIdUseCaseResult
 
 class FirebaseCardUseCaseImpl(
     private val cardRepository: CardRepository
 ) : CardUseCase {
+
     override suspend fun getAllCards(email: String): GetAllCardsUseCaseResult {
         return when (val getAllCardsRepositoryResult = cardRepository.getAllCards(email)) {
             is GetAllCardsRepositoryResult.Success -> {
@@ -15,10 +18,22 @@ class FirebaseCardUseCaseImpl(
                 val cardViewList = cardDomainList.map { cardDomain: CardDomain ->
                     cardDomain.convertToCardView()
                 }
-                GetAllCardsUseCaseResult.Success(cardViewList)
+                GetAllCardsUseCaseResult.SuccessWithCards(cardViewList)
             }
             is GetAllCardsRepositoryResult.ErrorUnknown -> {
                 GetAllCardsUseCaseResult.ErrorUnknown
+            }
+        }
+    }
+
+    override suspend fun getCardById(cardId: String): GetCardByIdUseCaseResult {
+        return when (val getCardByIdRepositoryResult = cardRepository.getCardById(cardId)) {
+            is GetCardByIdRepositoryResult.Success -> {
+                val cardView = getCardByIdRepositoryResult.cardDomain.convertToCardView()
+                GetCardByIdUseCaseResult.Success(cardView)
+            }
+            is GetCardByIdRepositoryResult.ErrorUnknown -> {
+                GetCardByIdUseCaseResult.ErrorUnknown
             }
         }
     }
