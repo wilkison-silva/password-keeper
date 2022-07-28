740afbb4158082f94f8e9446c5825b3314df90bd
Wilkison Silva <wilkisons@ciandt.com>
2022-07-28 14:56:05 -0400

    fix: utilizando apply pra diminuir o código

--- app/src/main/java/br/com/passwordkeeper/domain/usecase/PasswordValidationUseCaseImpl.kt
+++ app/src/main/java/br/com/passwordkeeper/domain/usecase/PasswordValidationUseCaseImpl.kt
@@ -14,23 +14,25 @@ class PasswordValidationUseCaseImpl : PasswordValidationUseCase {
     override fun validatePassword(password: String): PasswordValidationUseCaseResult {
         val errorList = mutableListOf<ErrorsValidationPassword>()
 
-        if (!password.contains(REGEX_UPPER_CASE.toRegex()))
-            errorList.add(ErrorsValidationPassword.ErrorOneUpperLetterNotFound)
+        errorList.apply {
+            if (!password.contains(REGEX_UPPER_CASE.toRegex()))
+                add(ErrorsValidationPassword.ErrorOneUpperLetterNotFound)
 
-        if (!password.contains(REGEX_LOWER_CASE.toRegex()))
-            errorList.add(ErrorsValidationPassword.ErrorOneLowerLetterNotFound)
+            if (!password.contains(REGEX_LOWER_CASE.toRegex()))
+                add(ErrorsValidationPassword.ErrorOneLowerLetterNotFound)
 
-        if (!password.contains(REGEX_SPECIAL_LETTER.toRegex()))
-            errorList.add(ErrorsValidationPassword.ErrorOneSpecialCharacterNotFound)
+            if (!password.contains(REGEX_SPECIAL_LETTER.toRegex()))
+                add(ErrorsValidationPassword.ErrorOneSpecialCharacterNotFound)
 
-        if (!password.contains(REGEX_NUMBER.toRegex()))
-            errorList.add(ErrorsValidationPassword.ErrorOneNumericCharacterNotFound)
+            if (!password.contains(REGEX_NUMBER.toRegex()))
+                add(ErrorsValidationPassword.ErrorOneNumericCharacterNotFound)
 
-        if (password.length < MIN_LENGTH_PASSWORD)
-            errorList.add(ErrorsValidationPassword.ErrorPasswordLengthNotMatch)
+            if (password.length < MIN_LENGTH_PASSWORD)
+                add(ErrorsValidationPassword.ErrorPasswordLengthNotMatch)
 
-        if (errorList.size > 0)
-            return PasswordValidationUseCaseResult.ErrorsFound(errorList)
+            if (size > 0)
+                return PasswordValidationUseCaseResult.ErrorsFound(errorList)
+        }
 
         return PasswordValidationUseCaseResult.Success
     }
