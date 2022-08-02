f77149e03e44770f8ce8a07f02d9f1d91afafa8b
Wilkison Silva <wilkisons@ciandt.com>
2022-08-02 12:15:43 -0400

    fix: refatorando nomes de variaveis e imports SignUpViewModel

--- app/src/main/java/br/com/passwordkeeper/presentation/ui/viewModel/SignUpViewModel.kt
+++ app/src/main/java/br/com/passwordkeeper/presentation/ui/viewModel/SignUpViewModel.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.passwordkeeper.domain.result.usecase.*
 import br.com.passwordkeeper.domain.result.usecase.ErrorsValidationPassword.*
-import br.com.passwordkeeper.domain.result.usecase.PasswordValidationUseCaseResult.*
 import br.com.passwordkeeper.domain.result.viewmodelstate.*
+import br.com.passwordkeeper.domain.result.viewmodelstate.FormValidationSignUpStateResult.*
 import br.com.passwordkeeper.domain.usecase.FormValidationSignUpUseCase
 import br.com.passwordkeeper.domain.usecase.PasswordValidationUseCase
 import br.com.passwordkeeper.domain.usecase.SignUpUseCase
@@ -16,7 +16,7 @@ import kotlinx.coroutines.launch
 class SignUpViewModel(
     private val signUpUseCase: SignUpUseCase,
     private val formValidationSignUpUseCase: FormValidationSignUpUseCase,
-    private val passwordValidationUseCase: PasswordValidationUseCase
+    private val passwordValidationUseCase: PasswordValidationUseCase,
 ) : ViewModel() {
 
     private val _createUserState = MutableLiveData<CreateUserStateResult>()
@@ -40,13 +40,13 @@ class SignUpViewModel(
     val passwordLowerLetterState: LiveData<ValidationStateResult>
         get() = _passwordLowerLetterState
 
-    private val _passwordSpecialCharacterState = MutableLiveData<ValidationStateResult>()
+    private val _passwordSpecialLetterState = MutableLiveData<ValidationStateResult>()
     val passwordSpecialCharacterState: LiveData<ValidationStateResult>
-        get() = _passwordSpecialCharacterState
+        get() = _passwordSpecialLetterState
 
-    private val _passwordNumericCharacterState = MutableLiveData<ValidationStateResult>()
+    private val _passwordNumericLetterState = MutableLiveData<ValidationStateResult>()
     val passwordNumericCharactersState: LiveData<ValidationStateResult>
-        get() = _passwordNumericCharacterState
+        get() = _passwordNumericLetterState
 
     private val _passwordLengthState = MutableLiveData<ValidationStateResult>()
     val passwordLengthState: LiveData<ValidationStateResult>
@@ -56,31 +56,23 @@ class SignUpViewModel(
         name: String,
         email: String,
         password: String,
-        confirmedPassword: String
+        confirmedPassword: String,
     ) {
-        val formValidationSignUpUseCaseResult =
-            formValidationSignUpUseCase.validateForm(name, email, password, confirmedPassword)
-        when (formValidationSignUpUseCaseResult) {
+        when (formValidationSignUpUseCase.validateForm(name, email, password, confirmedPassword)) {
             is FormValidationSignUpUseCaseResult.ErrorEmailIsBlank ->
-                _formValidationState.postValue(FormValidationSignUpStateResult.ErrorEmailIsBlank)
+                _formValidationState.postValue(ErrorEmailIsBlank)
             is FormValidationSignUpUseCaseResult.ErrorEmailMalFormed ->
-                _formValidationState.postValue(FormValidationSignUpStateResult.ErrorEmailMalFormed)
+                _formValidationState.postValue(ErrorEmailMalFormed)
             is FormValidationSignUpUseCaseResult.ErrorNameIsBlank ->
-                _formValidationState.postValue(FormValidationSignUpStateResult.ErrorNameIsBlank)
+                _formValidationState.postValue(ErrorNameIsBlank)
             is FormValidationSignUpUseCaseResult.ErrorPasswordIsBlank ->
-                _formValidationState.postValue(FormValidationSignUpStateResult.ErrorPasswordIsBlank)
+                _formValidationState.postValue(ErrorPasswordIsBlank)
             is FormValidationSignUpUseCaseResult.ErrorPasswordTooWeak ->
-                _formValidationState.postValue(FormValidationSignUpStateResult.ErrorPasswordTooWeak)
+                _formValidationState.postValue(ErrorPasswordTooWeak)
             is FormValidationSignUpUseCaseResult.ErrorPasswordsDoNotMatch ->
-                _formValidationState.postValue(FormValidationSignUpStateResult.ErrorPasswordsDoNotMatch)
+                _formValidationState.postValue(ErrorPasswordsDoNotMatch)
             is FormValidationSignUpUseCaseResult.Success ->
-                _formValidationState.postValue(
-                    FormValidationSignUpStateResult.Success(
-                        name,
-                        email,
-                        password
-                    )
-                )
+                _formValidationState.postValue(Success(name, email, password))
         }
 
     }
@@ -104,13 +96,13 @@ class SignUpViewModel(
 
     fun updateStatesToEmptyState() {
         _createUserState.postValue(CreateUserStateResult.EmptyState)
-        _formValidationState.postValue(FormValidationSignUpStateResult.EmptyState)
+        _formValidationState.postValue(EmptyState)
     }
 
     private fun setPasswordState(
         errorList: List<ErrorsValidationPassword>,
         error: ErrorsValidationPassword,
-        state: MutableLiveData<ValidationStateResult>
+        state: MutableLiveData<ValidationStateResult>,
     ) {
         errorList.contains(error).let {
             if (it)
@@ -123,25 +115,17 @@ class SignUpViewModel(
     fun updatePasswordValidation(password: String) {
         when (val passwordValidationUseCaseResult =
             passwordValidationUseCase.validatePassword(password)) {
-            is ErrorsFound -> {
+            is PasswordValidationUseCaseResult.ErrorsFound -> {
                 passwordValidationUseCaseResult.errorList.let {
                     setPasswordState(it, ErrorOneUpperLetterNotFound, _passwordUpperLetterState)
                     setPasswordState(it, ErrorOneLowerLetterNotFound, _passwordLowerLetterState)
-                    setPasswordState(
-                        it,
-                        ErrorOneSpecialCharacterNotFound,
-                        _passwordSpecialCharacterState
-                    )
-                    setPasswordState(
-                        it,
-                        ErrorOneNumericCharacterNotFound,
-                        _passwordNumericCharacterState
-                    )
-                    setPasswordState(it, ErrorPasswordLengthNotMatch, _passwordLengthState)
+                    setPasswordState(it, ErrorOneSpecialLetterNotFound, _passwordSpecialLetterState)
+                    setPasswordState(it, ErrorOneNumericLetterNotFound, _passwordNumericLetterState)
+                    setPasswordState(it, ErrorPasswordSizeNotMatch, _passwordLengthState)
                 }
             }
 
-            is PasswordFieldEmpty ->
+            is PasswordValidationUseCaseResult.PasswordFieldEmpty ->
                 _passwordFieldIsEmptyState.postValue(Unit)
         }
     }
