0d57e358bc5322f7c2fa2c864410fc4405fa1bb1
Wilkison Silva <wilkisons@ciandt.com>
2022-08-24 22:05:42 -0400

    feat: mudando nome da classe FormCardFragment para CreateNewCardFragment

--- app/src/main/java/br/com/passwordkeeper/presentation/ui/fragment/CreateNewCardFragment.kt
+++ app/src/main/java/br/com/passwordkeeper/presentation/ui/fragment/CreateNewCardFragment.kt
@@ -6,29 +6,27 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import br.com.passwordkeeper.R
-import br.com.passwordkeeper.databinding.FormCardFragmentBinding
+import br.com.passwordkeeper.databinding.CreateNewCardFragmentBinding
 import br.com.passwordkeeper.domain.model.Categories
 import br.com.passwordkeeper.domain.model.Categories.*
 import br.com.passwordkeeper.extensions.downloadImageDialog
 import br.com.passwordkeeper.presentation.ui.dialog.BottomSheetCategory
-import br.com.passwordkeeper.presentation.ui.viewmodel.HomeViewModel
 import br.com.passwordkeeper.presentation.ui.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class FormCardFragment : Fragment(R.layout.form_card_fragment) {
+class CreateNewCardFragment : Fragment(R.layout.create_new_card_fragment) {
 
     private val navController by lazy {
         findNavController()
     }
 
-    private lateinit var binding: FormCardFragmentBinding
+    private lateinit var binding: CreateNewCardFragmentBinding
     private val mainViewModel: MainViewModel by sharedViewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel.updateBottomNavigationVisibility(visibility = true)
-        binding = FormCardFragmentBinding.bind(view)
+        binding = CreateNewCardFragmentBinding.bind(view)
         binding.imageButtonBack.setOnClickListener {
             navController.popBackStack()
         }
