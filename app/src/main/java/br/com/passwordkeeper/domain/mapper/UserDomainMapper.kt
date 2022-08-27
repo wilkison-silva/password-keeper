d738b471c6984bbcda3bd225ec303866830dacee
Wilkison Silva <wilkisons@ciandt.com>
2022-08-27 18:34:29 -0400

    feat: cria mapper de UserDomain para UserView

---
+++ app/src/main/java/br/com/passwordkeeper/domain/mapper/UserDomainMapper.kt
@@ -0,0 +1,16 @@
+package br.com.passwordkeeper.domain.mapper
+
+import br.com.passwordkeeper.domain.model.UserDomain
+import br.com.passwordkeeper.domain.model.UserView
+
+class UserDomainMapper : BaseMapper<UserDomain, UserView>() {
+
+    override fun transform(model: UserDomain): UserView {
+        return UserView(
+            name = model.name,
+            firstCharacterName = model.name.substring(0, 1),
+            email = model.email
+        )
+    }
+
+}
