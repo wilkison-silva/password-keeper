18f1a7e01c821539f471dde3cfbe354abf1623f7
Wilkison Silva <wilkisons@ciandt.com>
2022-08-28 19:03:37 -0400

    feat: create usecase de validação de formulário de criação e edição de cards

---
+++ app/src/main/java/br/com/passwordkeeper/domain/usecase/FormValidationCardUseCaseImpl.kt
@@ -0,0 +1,33 @@
+package br.com.passwordkeeper.domain.usecase
+
+import br.com.passwordkeeper.domain.model.Categories
+import br.com.passwordkeeper.domain.result.usecase.FormValidationCardUseCaseResult
+
+class FormValidationCardUseCaseImpl() : FormValidationCardUseCase {
+
+    override fun validateForm(
+        description: String,
+        login: String,
+        password: String,
+        category: String,
+        isFavorite: Boolean,
+        date: String,
+        emailUser: String
+    ): FormValidationCardUseCaseResult {
+        if (description.isBlank())
+            return FormValidationCardUseCaseResult.DescriptionIsEmpty
+        if (!validateCategory(category))
+            return FormValidationCardUseCaseResult.CategoryNotSelected
+        return FormValidationCardUseCaseResult.Success
+    }
+
+    private fun validateCategory(category: String): Boolean {
+        return try {
+            Categories.valueOf(category)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+}
