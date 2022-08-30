31fbef5adb4b6e3eb55615a2887982716b1ef21e
CIANDT\francisn <francisn@ciandt.com>
2022-08-30 14:57:12 -0300

    feature: ocultando o bottom navigation da tela de erro ao criar um novo card

--- app/src/main/java/br/com/passwordkeeper/presentation/ui/fragment/NewNoteErrorFragment.kt
+++ app/src/main/java/br/com/passwordkeeper/presentation/ui/fragment/NewNoteErrorFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import br.com.passwordkeeper.R
 import br.com.passwordkeeper.databinding.NewNoteErrorBinding
+import br.com.passwordkeeper.presentation.ui.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class NewNoteErrorFragment: Fragment(R.layout.new_note_error){
 
@@ -14,9 +16,11 @@ class NewNoteErrorFragment: Fragment(R.layout.new_note_error){
     }
 
     private lateinit var binding: NewNoteErrorBinding
+    private val mainViewModel: MainViewModel by sharedViewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = NewNoteErrorBinding.bind(view)
+        mainViewModel.updateBottomNavigationVisibility(visibility = false)
     }
 }
