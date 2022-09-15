c281606f30be7753124257d72d124d96be164629
Wilkison Silva <wilkisons@ciandt.com>
2022-09-14 20:58:19 -0400

    feat: ajusta o nome do xml new note success fragment

--- app/src/main/java/br/com/passwordkeeper/presentation/ui/fragment/NewNoteSuccessFragment.kt
+++ app/src/main/java/br/com/passwordkeeper/presentation/ui/fragment/NewNoteSuccessFragment.kt
@@ -6,26 +6,25 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import br.com.passwordkeeper.R
-import br.com.passwordkeeper.databinding.NewNoteSuccessBinding
+import br.com.passwordkeeper.databinding.FragmentNewNoteSuccessBinding
+
 import br.com.passwordkeeper.presentation.ui.viewmodel.MainViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
-class NewNoteSuccessFragment : Fragment(R.layout.new_note_success) {
+class NewNoteSuccessFragment : Fragment(R.layout.fragment_new_note_success) {
 
     private val navController by lazy {
         findNavController()
     }
 
-    private lateinit var binding: NewNoteSuccessBinding
+    private lateinit var binding: FragmentNewNoteSuccessBinding
     private val mainViewModel: MainViewModel by sharedViewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = NewNoteSuccessBinding.bind(view)
+        binding = FragmentNewNoteSuccessBinding.bind(view)
         mainViewModel.updateBottomNavigationVisibility(visibility = false)
         lifecycleScope.launch {
             delay(2500)
